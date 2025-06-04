5ed09f1e3ee0b5eb5a593bf30ca5c837556b0b66
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-05 02:39:37 +0800

    Time: 0 ms (100%), Space: 19.4 MB (65.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size() == 0)   return true;
         vector<vector<int>> graph(numCourses);
-        for(auto pre: prerequisites){
-            graph[pre[1]].push_back(pre[0]);
+        for(auto& pre: prerequisites){
+            graph[pre[0]].push_back(pre[1]);
         }
-        vector<int> visit(numCourses, 0);
+        vector<int> state(numCourses, 0);
         for(int i=0; i<numCourses; i++){
-            if(!dfs(i, graph, visit))   return false;
+            if(!dfs(i, graph, state))   return false;
         }
         return true;
-    
     }
-    bool dfs(int current, vector<vector<int>>& graph, vector<int>& visit){
-        if(visit[current] == 1) return true;
-        else if(visit[current] == -1)   return false;
-        
-        visit[current] = -1;//visiting
-        for(int neighbor: graph[current]){
-            if(!dfs(neighbor, graph, visit))   return false;
+    bool dfs(int course, vector<vector<int>>& graph, vector<int>& state){
+        if(state[course] == 1)  return false;
+        else if(state[course] == 2) return true;
+        state[course] = 1;//visiting
+        for(auto neighbor: graph[course]){
+            if(!dfs(neighbor, graph, state))  return false;
         }
-        visit[current] = 1;//done
+        state[course] = 2;//done
         return true;
     }
 };
