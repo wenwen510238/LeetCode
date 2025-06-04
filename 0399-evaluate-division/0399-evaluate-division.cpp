1352e826a7eaedf7ab4923179ce520bf62decfc4
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-04 15:38:02 +0800

    Time: 0 ms (100%), Space: 12 MB (38.54%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,50 +1,35 @@
 class Solution {
 public:
+    unordered_map<string, vector<pair<string, double>>> graph;
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        unordered_map<string, vector<pair<string, double>>> graph;
         vector<double> res;
+        // build directed tree
         for(int i=0; i<equations.size(); i++){
             graph[equations[i][0]].push_back({equations[i][1], values[i]});
             graph[equations[i][1]].push_back({equations[i][0], 1.0 / values[i]});
         }
 
-        for(auto query: queries){
+        for(auto& query: queries){
             string start = query[0], end = query[1];
-            
-            if(graph.find(start) == graph.end() || graph.find(end) == graph.end()){
-                res.push_back(-1);
-                continue;
+            if(!graph.count(start) || !graph.count(end))    res.push_back(-1.0);
+            else if(start == end)   res.push_back(1.0);
+            else{
+                unordered_set<string> visit;
+                double result = dfs(start, end, visit, 1.0);
+                res.push_back(result);
             }
-            if(start == end){
-                res.push_back(1.0);
-                continue;
-            }
-        
-            queue<pair<string, double>> q;
-            unordered_set<string> visit;
-
-            q.push({start, 1});
-            visit.insert(start);
-            int found = false;
-            while(!q.empty()){
-                auto [node, val] = q.front();
-                q.pop();
-                for(auto& [neighbor, weight]: graph[node]){
-                    if(visit.count(neighbor))   continue;
-                    double newVal = val * weight;
-                    if(neighbor == end){
-                        res.push_back(newVal);
-                        found = true;
-                        break;
-                    }
-                        
-                    visit.insert(neighbor);
-                    q.push({neighbor, newVal});
-                }
-                if(found)   break;
-            }
-            if(!found)  res.push_back(-1.0);
         }
         return res;
+    }
+    double dfs(string start, string end, unordered_set<string>& visit, double product){
+        if(start == end)    return product;
+        visit.insert(start);
+
+        for(auto& [neighbor, weight]: graph[start]){
+            if(visit.count(neighbor))   continue;
+            double result = dfs(neighbor, end, visit, product * weight);
+            if(result != -1)   return result;      
+        }
+        return -1;
     }
 };
