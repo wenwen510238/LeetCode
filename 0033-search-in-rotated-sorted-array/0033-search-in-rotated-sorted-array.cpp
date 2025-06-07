154f43207d4494d060d945ab5fa543a5aac1296d
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-08 02:54:31 +0800

    Time: 0 ms (100%), Space: 15.2 MB (33.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        int peak;
-        // find peak
-        if(nums[left] < nums[right])    peak = right;
-        else{
-            while(right - left > 1){
-                int mid = (left + right) / 2;
-                if(nums[mid] < nums[right]) right = mid;
-                else left = mid;
-            }
-            if(nums[left] > nums[right])    peak = left;
-            else    peak = right;
-        }
-        
-        cout<<peak;
-        //divide two subset to find target
-        if(nums[peak] < target) return -1;
-        else if(target >= nums[0])   return find(target, 0, peak, nums);
-        else return find(target, peak+1, nums.size()-1, nums);
-    }
-    int find(int target, int left, int right, vector<int>& nums){
-        // if(left >= nums.size()) return -1;
+        int left = 0, right = nums.size()-1;
         while(left <= right){
             int mid = (left + right) / 2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+
+            
+            if(nums[left] <= nums[mid]){//左半邊遞增
+                if(nums[left] <= target && target < nums[mid])  right = mid-1;
+                else left = mid + 1;
+            }
+            else{//右半邊遞增
+                if(nums[mid] < target && target <= nums[right])  left = mid+1;
+                else right = mid - 1;
+            }
         }
         return -1;
     }
 };
-
-// 5 6 7 0 1 2 3
-// 7 0 1 2 3 5 6 
