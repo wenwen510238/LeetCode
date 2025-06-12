42c3d7b297f3d113d8e3be628eb066530e384c48
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-13 00:45:00 +0800

    Time: 80 ms (46.45%), Space: 14.4 MB (47.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -7,7 +7,6 @@ public:
         for(int i=1; i<n; i++){
             for(int j=i-1; j>=0; j--){
                 if(nums[j] < nums[i])   dp[i] = max(dp[j]+1, dp[i]);
-                else if(nums[j] == nums[i]) dp[i] = max(dp[j], dp[i]);
             }
             res = max(res, dp[i]);
         }
