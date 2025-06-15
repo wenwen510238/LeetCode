2fa89678f53f2a1bdbef8f0c5149f27668de15a4
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-15 17:54:30 +0800

    Time: 28 ms (51.65%), Space: 16 MB (99.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int land = 0;
+        int res = 0;
+        int n = grid.size(), m = grid[0].size();
         for(int i=0; i<grid.size(); i++){
-            for(int j=0; j<grid[i].size(); j++){
+            for(int j=0; j<grid[0].size(); j++){
                 if(grid[i][j] == '1'){
-                    land++;
-                    FindLand(grid, i, j);
+                    int len = 0;
+                    dfs(grid, i, j);
+                    res++;
                 }
             }
         }
-        return land;
+        return res;
     }
-    void FindLand(vector<vector<char>>& grid, int row, int col){
-        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || grid[row][col] != '1')   return;
-        grid[row][col] = '0';
+    void dfs(vector<vector<char>>& grid, int row, int col){
+        if(row<0 || row>=grid.size() || col<0 || col>=grid[0].size() || grid[row][col] == '0')  return ;
+        if(grid[row][col] == '1')   grid[row][col] = '0';
 
-        FindLand(grid, row+1, col);
-        FindLand(grid, row, col+1);
-        FindLand(grid, row-1, col);
-        FindLand(grid, row, col-1);
+        dfs(grid, row+1, col);
+        dfs(grid, row, col+1);
+        dfs(grid, row-1, col);
+        dfs(grid, row, col-1);
     }
 };
