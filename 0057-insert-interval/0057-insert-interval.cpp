1702e9289290d00c2e8e0113fb9f25f97fb69246
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-26 21:35:03 +0800

    Time: 3 ms (31.61%), Space: 21.6 MB (93.79%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,17 +4,21 @@ public:
         vector<vector<int>> res;
         int n = intervals.size();
         int current = 0;
+        //handle left part of newInterval
         while(current < n && newInterval[0] > intervals[current][1]){
             res.push_back(intervals[current]);
             current++;
         }
 
+        //handle overlapping part
         while(current < n && newInterval[1] >= intervals[current][0]){
             newInterval[0] = min(newInterval[0], intervals[current][0]);
             newInterval[1] = max(newInterval[1], intervals[current][1]);
             current++;
         }
         res.push_back({newInterval[0], newInterval[1]});
+
+        //handle right part of newInterval
         while(current < n){
             res.push_back(intervals[current]);
             current++;
