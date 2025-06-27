dc4a58e28dfd05b9a1fc5b3c5a53d60aaacdcf36
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-28 02:59:15 +0800

    Time: 0 ms (100%), Space: 14.4 MB (40.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -12,12 +12,13 @@ public:
             res.push_back(path);
             return;
         }
-        if(target < 0) return;
+        if(target < 0 || idx >= candidates.size()) return;
 
-        for(int i=idx; i<candidates.size(); i++){
-            path.push_back(candidates[i]);
-            dfs(candidates, target-candidates[i], path , i);
-            path.pop_back();
-        }
+        // for(int i=idx; i<candidates.size(); i++){
+        path.push_back(candidates[idx]);
+        dfs(candidates, target-candidates[idx], path , idx);
+        path.pop_back();
+        dfs(candidates, target, path , idx+1);
+        // }
     }
 };
