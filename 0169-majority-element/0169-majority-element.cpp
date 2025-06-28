088f2c8a9fdfa5705e209acb9afa6f33c3d354bd
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-06-28 20:43:30 +0800

    Time: 0 ms (100%), Space: 28.2 MB (24.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = 0, cnt = 0;
-        for(int num:nums){
-            if(cnt == 0)    candidate = num;
-            if(num == candidate)    cnt++;
-            else    cnt--;
+        int candidate;
+        int cnt = 0;
+        for(auto& num: nums){
+            if(cnt == 0) candidate = num;
+            if(num != candidate)   cnt++;
+            else cnt--;
         }
         return candidate;
     }
