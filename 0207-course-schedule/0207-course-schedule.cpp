8eff5bd41550e399b42a2ac2535186d7d80e7e92
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-07-15 01:41:44 +0800

    Time: 10 ms (25.79%), Space: 20.2 MB (31.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size() == 0)   return true;
-        vector<vector<int>> graph(numCourses);
-        vector<int> inDegree(numCourses, 0);
-        for(auto pre: prerequisites){
+        unordered_map<int, vector<int>> graph;
+        vector<int> visit(numCourses, 0);
+        for(auto& pre: prerequisites){
             graph[pre[1]].push_back(pre[0]);
-            inDegree[pre[0]]++;
         }
-        queue<int> q;
-        int complete = 0;
+
         for(int i=0; i<numCourses; i++){
-            if(inDegree[i] == 0)   q.push(i);
+            if(!dfs(i, graph, visit))  return false;
         }
-        while(!q.empty()){
-            int current = q.front();
-            q.pop();
-            complete++;
-            for(auto& neighbor: graph[current]){
-                inDegree[neighbor]--;
-                if(!inDegree[neighbor]) q.push(neighbor);
-            }
+        return true;
+    }  
+    bool dfs(int numCourses, unordered_map<int, vector<int>>& graph, vector<int>& visit){
+        if(visit[numCourses] == 1)  return false;
+        if(visit[numCourses] == 2)  return true;
+        
+        visit[numCourses] = 1; // visiting
+
+        for(auto& nei: graph[numCourses]){
+            if(!dfs(nei, graph, visit))    return false;
         }
-        return complete == numCourses;
-    
+        visit[numCourses] = 2;// finish
+        return true;
     }
 };
