78ad3d9cb165265c00d762939f28d4bf5561a1b6
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-07-20 00:24:11 +0800

    Time: 46 ms (34.34%), Space: 44 MB (26.81%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,42 +1,68 @@
 class Solution {
 public:
+    vector<int> parent, rank;
+    unordered_map<string, int> emailToId;
+    vector<string> idToEmail;               
     unordered_map<string, string> emailToName;
-    unordered_map<string, string> parent;
 
-    string find(string s){
-        if(parent[s] != s) parent[s] = find(parent[s]);  
-        return parent[s];
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);    
+        return parent[x];
     }
 
-    void unite(string a, string b){
-        parent[find(a)] = find(b);
+    void unite(int a, int b) {
+        int ra = find(a), rb = find(b);
+        if (ra == rb) return;
+        if (rank[ra] < rank[rb]) {
+            parent[ra] = rb;
+        } else if (rank[ra] > rank[rb]) {
+            parent[rb] = ra;
+        } else {
+            parent[rb] = ra;
+            rank[ra]++;
+        }
     }
 
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        for(auto& acc: accounts){
-            for(int i=1; i<acc.size(); i++){
-                emailToName[acc[i]] = acc[0];
-                parent[acc[i]] = acc[i];
+        int id = 0;
+        // Step 1：為每個 email 指派整數 id
+        for (auto& acc : accounts) {
+            string name = acc[0];
+            for (int i = 1; i < acc.size(); ++i) {
+                string& email = acc[i];
+                if (!emailToId.count(email)) {
+                    emailToId[email] = id++;
+                    idToEmail.push_back(email);
+                    parent.push_back(parent.size());
+                    rank.push_back(0);
+                }
+                emailToName[email] = name;
             }
         }
 
-        for(auto& acc: accounts){
+        // Step 2：同帳號內的 email 做 union
+        for (auto& acc : accounts) {
+            int first = emailToId[acc[1]];
             for (int i = 2; i < acc.size(); ++i) {
-                unite(acc[i - 1], acc[i]);
+                unite(first, emailToId[acc[i]]);
             }
         }
 
-        unordered_map<string, vector<string>> rootToEmails;
-        for(auto& [email, _]: emailToName){
-            string root = find(email);
+        // Step 3：以 root 分組
+        unordered_map<int, vector<string>> rootToEmails;
+        for (auto& [email, idx] : emailToId) {
+            int root = find(idx);
             rootToEmails[root].push_back(email);
         }
 
+        // Step 4：組裝答案
         vector<vector<string>> res;
-        for(auto& [root, emails]: rootToEmails){
-            sort(emails.begin(), emails.end());
-            emails.insert(emails.begin(), emailToName[root]);
-            res.push_back(emails);
+        res.reserve(rootToEmails.size());
+        for (auto& [root, vec] : rootToEmails) {
+            sort(vec.begin(), vec.end());
+            vec.insert(vec.begin(), emailToName[vec[0]]);
+            res.push_back(std::move(vec));
         }
         return res;
     }
