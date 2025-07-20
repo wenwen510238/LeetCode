770305908c50b29bbf251c3e28c4602c8bb59069
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-07-20 18:06:09 +0800

    Time: 71 ms (22.14%), Space: 65.1 MB (20.91%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         if(n == 1)  return {0};
-        vector<vector<int>> graph(n);
         vector<int> degree(n, 0);
+        unordered_map<int, vector<int>> graph;
+
         for(auto& edge: edges){
-            degree[edge[0]]++;
-            degree[edge[1]]++;
+            degree[edge[0]] ++;
+            degree[edge[1]] ++;
             graph[edge[0]].push_back(edge[1]);
             graph[edge[1]].push_back(edge[0]);
         }
 
-        queue<int> leaf;
+        queue<int> q;
         for(int i=0; i<n; i++){
-            if(degree[i] == 1)   leaf.push(i);   
+            if(degree[i] == 1)  q.push(i);
         }
 
         vector<int> res;
-        while(!leaf.empty()){
-            int size = leaf.size();
+        while(!q.empty()){
+            int size = q.size();
             res.clear();
             for(int i=0; i<size; i++){
-                int current = leaf.front();
-                leaf.pop();
-                res.push_back(current);
-                for(auto nei: graph[current]){
+                int curr = q.front();
+                q.pop();
+                res.push_back(curr);
+                for(auto& nei: graph[curr]){
                     degree[nei]--;
-                    if(degree[nei] == 1)    leaf.push(nei);
+                    if(degree[nei] == 1) q.push(nei);
                 }
             }
         }
