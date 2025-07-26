b9d43d9559ae64f82581465d7f6411287dec1120
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-07-26 22:40:44 +0800

    Time: 0 ms (100%), Space: 21.1 MB (47.69%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,27 +11,32 @@
  */
 class Solution {
 public:
+    vector<vector<int>> res;
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> res;
-        vector<int> curPath;
-        dfs(root, targetSum, res, curPath);
+        if(!root) return {};
+        vector<int> path;
+        dfs(root, targetSum, path);
         return res;
     }
 
-    void dfs(TreeNode* root, int targetSum,  vector<vector<int>>& res, vector<int>& curPath){
-        if(!root)   return;
+    void dfs(TreeNode* root, int target, vector<int>& path){
+        target -= root->val;
+        path.push_back(root->val);
 
-        targetSum -= root->val;
-        curPath.push_back(root->val);
-
-        if(!root->left && !root->right && targetSum == 0){
-            res.push_back(curPath);
+        if(!root->left && !root->right){
+            if(target == 0){
+                res.push_back(path);
+            }
+            return;
+        } 
+        if(root->left){
+            dfs(root->left, target, path);
+            path.pop_back();
         }
-        else{
-            dfs(root->left, targetSum, res, curPath);
-            dfs(root->right, targetSum, res, curPath);
-        }
-        curPath.pop_back();
+        if(root->right){
+            dfs(root->right, target, path);
+            path.pop_back();
 
+        }
     }
 };
