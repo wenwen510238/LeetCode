ebb0fa9446c21ae14cee531e9c033e4fffc355ee
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-07-28 14:49:39 +0800

    Time: 3 ms (32.56%), Space: 9.6 MB (99.54%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<bool> used(nums.size(), false);
         vector<int> path;
-        backtracking(nums, res, path, used, 0);
+        backtracking(nums, res, path, 0);
         return res;
     }
 
-    void backtracking(vector<int>& nums, vector<vector<int>>& res, vector<int>& path, vector<bool>& used, int idx){
+    void backtracking(vector<int>& nums, vector<vector<int>>& res, vector<int>& path, int idx){
         res.push_back(path);
         for(int i=idx; i<nums.size(); i++){
-            if(used[i]) continue;
-
-            used[i] = true;
             path.push_back(nums[i]);
 
-            backtracking(nums, res, path, used, i+1);
+            backtracking(nums, res, path, i+1);
             path.pop_back();
-            used[i] = false;
         }
     }
 };
