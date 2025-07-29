6a0c9e5c601d5044b3857eb5ed88301860234512
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-07-29 19:22:53 +0800

    Time: 0 ms (100%), Space: 19.4 MB (87.14%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,32 +11,24 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* Merge;
-        ListNode* MergeCurr;
-        if(list1 == NULL)   return list2;
-        else if(list2 == NULL)   return list1;
-        if(list1->val <= list2->val) {
-            Merge = new ListNode(list1->val);
-            list1 = list1->next;
-        }
-        else{
-            Merge = new ListNode(list2->val);
-            list2 = list2->next;
-        }
-        MergeCurr = Merge;
-        while((list1 != NULL) && (list2 != NULL)){
-            if(list1->val <= list2->val ){
-                MergeCurr->next = list1;
+        ListNode dummy(0);
+        ListNode* node = &dummy;
+
+        while(list1 && list2){
+            if(list1->val <= list2->val){
+                node->next = list1;
                 list1 = list1->next;
             }
             else{
-                MergeCurr->next = list2;
+                node->next = list2;
                 list2 = list2->next;
             }
-            MergeCurr =  MergeCurr->next;
+            node = node->next;
         }
-        if(list1 != NULL)    MergeCurr->next = list1;
-        if(list2 != NULL)    MergeCurr->next = list2;
-        return Merge;
+
+        if(list1) node->next = list1;
+        else if(list2) node->next = list2;
+
+        return dummy.next;
     }
 };
