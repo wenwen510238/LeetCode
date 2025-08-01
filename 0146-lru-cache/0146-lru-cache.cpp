709e7708358d4a7c011e98ed5500e6c694c3c777
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-01 11:10:32 +0800

    Time: 88 ms (51.46%), Space: 182.1 MB (37.57%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,40 +1,35 @@
 class LRUCache {
 public:
-    list<pair<int, int>> node;
-    unordered_map<int, list<pair<int, int>>::iterator> cache;
+    list<pair<int, int>> cache;
+    unordered_map<int, list<pair<int, int>>::iterator> mp; //key - list_idx
+    int cap;
     LRUCache(int capacity) {
         cap = capacity;
     }
     
     int get(int key) {
-        if(cache.find(key) == cache.end())  return -1;
-        auto iter = cache[key];
-        int val = iter->second;
-        node.erase(iter);
-        node.push_front({key, val});
-        cache[key] = node.begin();
-        return val;
+        if(mp.count(key)){
+            int val = mp[key]->second;
+            cache.erase(mp[key]);
+            cache.push_front({key, val});
+            mp[key] = cache.begin();
+            return val;
+        }
+        else return -1;
     }
     
     void put(int key, int value) {
-        if(cache.find(key) != cache.end()){
-            auto iter = cache[key];
-            node.erase(iter);
-            node.push_front({key, value});
-            cache[key] = node.begin();
+        if(mp.count(key)){
+            cache.erase(mp[key]);
         }
-        else{
-            if(cache.size() == cap){
-                int oldKey = node.back().first;
-                node.pop_back();
-                cache.erase(oldKey);
-            }
-            node.push_front({key, value});
-            cache[key] = node.begin();
+        else if(cache.size() == cap){
+            auto [k, _] = cache.back();
+            mp.erase(k);
+            cache.pop_back();
         }
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
     }
-private:
-    int cap;
 };
 
 /**
