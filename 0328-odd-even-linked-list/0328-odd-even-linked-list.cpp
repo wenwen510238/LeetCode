0249429c4f9cde9edaa3467fa4cd52b859f60c15
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-03 23:14:34 +0800

    Time: 0 ms (100%), Space: 15.7 MB (8.55%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -19,13 +19,17 @@ public:
 
         bool isOdd = false;
         while(head && head->next){
+            
+            // 判斷是不是奇數node，奇數node要記錄最後一個node，因為最後要接回偶數node
             isOdd = !isOdd;
             if(isOdd) oddNode = head;
+
             ListNode* tmp = head->next;
             head->next = head->next->next;
             head = tmp;
         }
 
+        // 如果結束的時候停在奇數node，要更新奇數node最後位置
         if(!isOdd) oddNode = head;
         
         oddNode->next = evenNode;
