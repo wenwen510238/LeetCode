d07645534b36aff9468ac344ff1f5cb77cced14e
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-05 23:29:46 +0800

    Time: 2 ms (12.44%), Space: 9 MB (14.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -9,13 +9,13 @@ public:
         for(auto& c: s){
             if(c == '(' || c == '{' || c == '[') stk.push(c);
             else{
-                if(stk.size() == 0) return false;
+                if(stk.empty()) return false;
                 char top = stk.top();
                 if(c != bracketMapping[top]) return false;
                 else stk.pop();
             }
         }
-        if(stk.size() == 0) return true;
+        if(stk.empty()) return true;
         return false;
     }
 };
