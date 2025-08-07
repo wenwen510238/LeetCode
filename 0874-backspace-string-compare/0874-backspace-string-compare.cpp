dc9b76cf195456a4cf90e8a0b40779a03a61ead6
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-08 02:46:43 +0800

    Time: 0 ms (100%), Space: 8.5 MB (71.63%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> ss;
-        stack<char> ts;
-        for(char c: s){
-            if(c != '#') ss.push(c);
-            else if(!ss.empty()) ss.pop();
+        int i=s.size()-1, j = t.size()-1;
+        int skipS = 0, skipT = 0;
+        while(i >= 0 || j >= 0){
+            while(i >= 0){
+                if(s[i] == '#'){
+                    i--;
+                    skipS++;
+                }
+                else if(skipS > 0){
+                    i--;
+                    skipS--;
+                }
+                else break;
+            }
+            while(j >= 0){
+                if(t[j] == '#'){
+                    j--;
+                    skipT++;
+                }
+                else if(skipT > 0){
+                    j--;
+                    skipT--;
+                }
+                else break;
+            }
+
+            if(i >= 0 && j >= 0){
+                if(s[i] != t[j]) return false;
+            }
+            else if(i >= 0 || j >= 0) return false;
+            i--;
+            j--;
         }
-         for(char c: t){
-            if(c != '#') ts.push(c);
-            else if(!ts.empty()) ts.pop();
-        }
-        if(ts == ss) return true;
-        return false;
+        return true;
     }
 };
