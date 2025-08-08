689563f78b8a8b4c356d516b0be4a9b4c58da67b
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-08 17:41:34 +0800

    Time: 16 ms (82.46%), Space: 107.1 MB (59.06%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
-        unordered_map<int, vector<int>> mp;
         vector<int> res(n, 0);
         stack<int> st;
     
         for(int i=0; i<n; i++){
-            while(!st.empty() && temperatures[i] > st.top()){
-                int idx = mp[st.top()].back();
-                res[idx] = i - idx;
-                mp[st.top()].pop_back();
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                res[st.top()] = i - st.top();
                 st.pop();
             }
-            mp[temperatures[i]].push_back(i);
-            st.push(temperatures[i]);
+            st.push(i);
         }
         return res;
     }
