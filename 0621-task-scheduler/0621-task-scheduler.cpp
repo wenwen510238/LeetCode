985cecba5647d7ae80dfe3aa86f3161c17f79d3c
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-10 01:17:49 +0800

    Time: 20 ms (52.67%), Space: 43 MB (48.77%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -5,16 +5,27 @@ public:
         for(auto c: tasks){
             letter[c-'A']++;
         }
-        int maxf = 0;
+        priority_queue<int> pq;
         for(int i: letter){
-            maxf = max(maxf, i);
+            if(i != 0) pq.push(i);
         }
-        int cntMax = 0;
-        for(int i: letter){
-            if(i == maxf) cntMax++;
+
+        queue<pair<int, int>> cool;
+        int time = 0;
+        while(!pq.empty() || !cool.empty()){
+            time++;
+
+            if(!pq.empty()){
+                int c = pq.top();
+                pq.pop();
+                if(--c > 0) cool.push({time + n, c});
+            }
+
+            if(!cool.empty() && cool.front().first == time){
+                pq.push(cool.front().second);
+                cool.pop();
+            }
         }
-        int res = (maxf - 1) * (n + 1) + cntMax;
-        int size = tasks.size();
-        return max(size, res);
+        return time;
     }
 };
