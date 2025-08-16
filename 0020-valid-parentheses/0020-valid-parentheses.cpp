e7dd883bca013a2a5e3b79da6584b476d095f4dc
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-16 20:09:36 +0800

    Time: 0 ms (100%), Space: 8.8 MB (60.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> stk;
-        unordered_map<char, char> bracketMapping;
-        bracketMapping['{'] = '}';
-        bracketMapping['['] = ']';
-        bracketMapping['('] = ')';
-        for(auto& c: s){
-            if(c == '(' || c == '{' || c == '[') stk.push(c);
+        for(auto c: s){
+            if(c == '{' || c == '(' || c == '[') stk.push(c);
             else{
-                if(stk.empty()) return false;
-                char top = stk.top();
-                if(c != bracketMapping[top]) return false;
-                else stk.pop();
+                switch(c){
+                    case '}':
+                        if(!stk.empty() && stk.top() == '{') stk.pop();
+                        else return false;
+                        break;
+                    case ')':
+                        if(!stk.empty() && stk.top() == '(') stk.pop();
+                        else return false;
+                        break;
+                    case ']':
+                        if(!stk.empty() && stk.top() == '[') stk.pop();
+                        else return false;
+                        break;
+                }
             }
         }
-        if(stk.empty()) return true;
-        return false;
+        if(!stk.empty()) return false;
+        return true;
     }
 };
