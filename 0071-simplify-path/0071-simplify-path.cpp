e86ab36819f3baa701fa6282b65681030b586d2f
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-16 21:03:03 +0800

    Time: 11 ms (7.06%), Space: 15.1 MB (32.44%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> stk;
+        string current = "";
+        string res = "";
+        for(int i=0; i<path.size(); i++){
+            if(path[i] == '/'){
+                if(current == ".." && !stk.empty()) stk.pop();
+                else if(current != "" && current != "." && current != ".." && current.find('/') == string::npos) stk.push(current);
+                current = "";
+                while(i+1<path.size() && path[i+1] == '/') i++;
+            }
+            else current += path[i];
+        }
+
+        if(current == ".." && !stk.empty()) stk.pop();
+        else if(current != "" && current != "." && current != ".." && current.find('/') == string::npos) stk.push(current);
+
+        while(!stk.empty()){
+            cout<<stk.top()<<" ";
+            res = "/" + stk.top() + res;
+            stk.pop();
+        }
+        if(res == "") return "/";
+        return res;
+    }
+};
