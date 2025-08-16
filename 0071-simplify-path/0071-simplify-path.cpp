8ed1ddcfb146527403faa4ee29f07e5e51db26e5
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-16 21:28:32 +0800

    Time: 4 ms (45.69%), Space: 11.1 MB (66.16%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        stack<string> stk;
+        vector<string> parts;
         string current = "";
-        string res = "";
-        for(int i=0; i<path.size(); i++){
-            if(path[i] == '/'){
-                if(current == ".." && !stk.empty()) stk.pop();
-                else if(current != "" && current != "." && current != "..") stk.push(current);
+        string res = "/";
+        for(int i=0; i<=path.size(); i++){
+            if(i == path.size() || path[i] == '/'){
+                if(current == ".." && !parts.empty()) parts.pop_back();
+                else if(current != "" && current != "." && current != "..") parts.push_back(current);
                 current = "";
                 while(i+1<path.size() && path[i+1] == '/') i++;
             }
             else current += path[i];
         }
 
-        if(current == ".." && !stk.empty()) stk.pop();
-        else if(current != "" && current != "." && current != "..") stk.push(current);
-
-        while(!stk.empty()){
-            res = "/" + stk.top() + res;
-            stk.pop();
+        for(int i=0; i<parts.size(); i++){
+            res += parts[i] + '/' ;
         }
-        if(res == "") return "/";
-        return res;
+        if(res == "/") return res;
+        return res.substr(0, res.size()-1);
     }
 };
