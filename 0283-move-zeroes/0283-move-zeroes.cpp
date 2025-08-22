ca23b8c83cd913c661301c5a4fdc7aa28f1965b3
wenwen510238 <94843261+wenwen510238@users.noreply.github.com>
2025-08-23 02:36:45 +0800

    Time: 0 ms (100%), Space: 23.9 MB (20.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int current = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                nums[current] = nums[i];
+                current++;
+            }
+        }
+        for(int i=current; i<nums.size(); i++){
+            nums[i] = 0;
+        }
+    }
+};
